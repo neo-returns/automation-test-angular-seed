6e6aaa0efa8bfcdedcc834af8c07bf32e3bbef2c
Paul Taylor <paultaylor@PaulTaylor.local>
2014-10-07 10:55:53 -0500

    Configuring code coverage; removed shippable.yml

--- config/karma-e2e.conf.js
+++ config/karma-e2e.conf.js
@@ -27,12 +27,26 @@ module.exports = function(config){
             'karma-jasmine',
             'karma-ng-scenario',
             'karma-phantomjs-launcher',
+            'karma-threshold-reporter'
             ],
 
     junitReporter : {
       outputFile: 'test_out/e2e.xml',
       suite: 'e2e'
     }
+	
+	reporters: ['progress', 'coverage','threshold'],
+	
+	coverageReporter: 
+  		type: "html"
+  		dir: process.env ['CIRCLE_ARTIFACTS'] + "/ coverage"
 
+    // the configure thresholds
+    thresholdReporter: {
+      statements: 90,
+      branches: 60,
+      functions: 85,
+      lines: 90
+    }
 })}
 
--- config/karma.conf.js
+++ config/karma.conf.js
@@ -28,11 +28,27 @@ module.exports = function(config){
             'karma-firefox-launcher',
             'karma-jasmine',
             'karma-phantomjs-launcher',
+            'karma-threshold-reporter'
             ],
 
     junitReporter : {
       outputFile: 'test_out/unit.xml',
       suite: 'unit'
     }
+    
+    reporters: ['progress', 'coverage','threshold'],
+    
+    coverageReporter: 
+  		type: "html"
+  		dir: process.env ['CIRCLE_ARTIFACTS'] + "/ coverage"
+
+    // the configure thresholds
+    thresholdReporter: {
+      statements: 90,
+      branches: 60,
+      functions: 85,
+      lines: 90
+    }
 
 })}
+
